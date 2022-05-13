9f935c988e9cbe3cfe53e0785e7eb21fdda468f3
Jeremy Theintz <jeremy@appogee.ch>
2022-05-13 22:32:58 +0200

    add new variables to calculate power consumption of proof of stake blockchains

--- packages/database/query/02-dump.sql
+++ packages/database/query/02-dump.sql
@@ -36,6 +36,9 @@ CREATE TABLE IF NOT EXISTS `blockmetrics-db`.`blockchain` (
   `description_en` TEXT NULL DEFAULT NULL,
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `consensus` VARCHAR(20) NOT NULL,
+  `testnet_node_count` INT UNSIGNED NULL DEFAULT NULL,
+  `single_node_power_consumption` FLOAT NULL DEFAULT 0,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
@@ -152,11 +155,11 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 INSERT INTO `blockchain` (`id`, `name`, `logoUrl`, `note`, `power_consumption`, `node_count`, `hashrate`,`difficulty`, `time_between_blocks`, `token_count`, `transaction_count`, `gas_price`, `description_en`, `created_at`, `updated_at`) VALUES
-('0bb6df38-231e-47d3-b427-88d16a65580e', 'Binance SC', '/assets/img/binance-smart-chain.svg', NULL, 0, NULL, 0, NULL, NULL, 0, 0, NULL, NULL, '2022-04-19 11:47:09', '2022-04-19 11:47:09'),
-('1daa2a79-98cc-49a5-970a-0ad620a8b0d9', 'Bitcoin', '/assets/img/bitcoin.svg', NULL, 0, NULL, 0, NULL, NULL, 1, 0, NULL, NULL, '2022-04-19 11:48:19', '2022-04-19 11:48:19'),
-('387123e4-6a73-44aa-b57e-79b5ed1246d4', 'Ethereum', '/assets/img/ethereum.svg', NULL, 0, NULL, 0, NULL, NULL, 0, 0, NULL, NULL, '2022-04-19 11:46:20', '2022-04-19 11:46:20'),
-('4df0b4ad-2165-4543-a74b-7cdf46f9c5e3', 'Polygon', '/assets/img/polygon-pos.svg', NULL, 0, NULL, 0, NULL, NULL, 0, 0, NULL, NULL, '2022-04-19 11:47:32', '2022-04-19 11:47:32'),
-('7fc003e2-680f-4e69-9741-b00c18d2e6dc', 'Avalanche', '/assets/img/avalanche.svg', NULL, 0, NULL, 0, NULL, NULL, 0, 0, NULL, NULL, '2022-04-19 11:50:53', '2022-04-19 11:50:53');
+('0bb6df38-231e-47d3-b427-88d16a65580e', 'Binance SC', '/assets/img/binance-smart-chain.svg', NULL, 0, NULL, 0, NULL, NULL, 0, 0, NULL, NULL, '2022-04-19 11:47:09', '2022-04-19 11:47:09', 'pos', 0, 0),
+('1daa2a79-98cc-49a5-970a-0ad620a8b0d9', 'Bitcoin', '/assets/img/bitcoin.svg', NULL, 0, NULL, 0, NULL, NULL, 1, 0, NULL, NULL, '2022-04-19 11:48:19', '2022-04-19 11:48:19', 'pow', 0, 0),
+('387123e4-6a73-44aa-b57e-79b5ed1246d4', 'Ethereum', '/assets/img/ethereum.svg', NULL, 0, NULL, 0, NULL, NULL, 0, 0, NULL, NULL, '2022-04-19 11:46:20', '2022-04-19 11:46:20', 'pow', 0, 0),
+('4df0b4ad-2165-4543-a74b-7cdf46f9c5e3', 'Polygon', '/assets/img/polygon-pos.svg', NULL, 0, NULL, 0, NULL, NULL, 0, 0, NULL, NULL, '2022-04-19 11:47:32', '2022-04-19 11:47:32', 'pos', 0, 0),
+('7fc003e2-680f-4e69-9741-b00c18d2e6dc', 'Avalanche', '/assets/img/avalanche.svg', NULL, 0, NULL, 0, NULL, NULL, 0, 0, NULL, NULL, '2022-04-19 11:50:53', '2022-04-19 11:50:53', 'pos', 0, 0);
 
 INSERT INTO `block_parsed` (`blockchain_id`, `number`) VALUES
 ('0bb6df38-231e-47d3-b427-88d16a65580e', 0),
