e60ce9247a96161228dfff1bd1d277679ded144f
Jeremy Theintz <jeremy@appogee.ch>
2022-07-17 03:49:51 +0200

    add awesome 3D animation for the burger button

--- packages/frontend/components/Navbar/Navbar.styles.ts
+++ packages/frontend/components/Navbar/Navbar.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { EMediaQuery } from '../../styles/theme/utils/enum';
 import { addTransition, mq } from '../../styles/theme/utils/functions';
 
@@ -48,5 +48,126 @@ export const StyledNavbarItem = styled.li`
 
 	&:nth-last-child(1) a {
 		margin-right: 0;
+	}
+`;
+
+export const StyledBurgerButton = styled.button`
+	position: relative;
+
+	width: 40px;
+	height: 40px;
+
+	padding: 0;
+
+	background-color: transparent;
+	border: none;
+
+	perspective: 500px;
+
+	cursor: pointer;
+`;
+
+export const StyledCube = styled.div<{ value: number }>`
+	position: absolute;
+
+	top: 50%;
+	left: 50%;
+
+	width: 0px;
+	height: 0px;
+	transform-style: preserve-3d;
+
+	${addTransition()}
+
+	transform: ${({ value }) => {
+		switch (value) {
+			case 0:
+				return 'rotateY(0deg) rotateX(0deg) rotateZ(0deg);';
+			case 1:
+				return 'rotateY(-90deg) rotateX(90deg) rotateZ(0deg);';
+			case 2:
+				return 'rotateY(180deg) rotateX(0deg) rotateZ(180deg);';
+			case 3:
+				return 'rotateY(270deg) rotateX(90deg) rotateZ(180deg);';
+			case 4:
+				return 'rotateY(90deg) rotateX(180deg) rotateZ(180deg);';
+			case 5:
+				return 'rotateY(270deg) rotateX(180deg) rotateZ(-90deg);';
+			case 6:
+				return 'rotateY(0deg) rotateX(0deg) rotateZ(0deg);';
+			default:
+				return 'rotateY(90deg) rotateX(0deg) rotateZ(-90deg);';
+		}
+	}};
+`;
+
+export const StyledFace = styled.div<{ faceIndex: number; faceVisible: boolean }>`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	position: absolute;
+
+	margin: -20px;
+	width: 40px;
+	height: 40px;
+
+	${({ faceIndex }) => {
+		const i = faceIndex > 5 ? 1 : 0;
+		const m = faceIndex < 5 ? faceIndex - 1 : (-1) ** (faceIndex - 1);
+
+		return css`
+			transform: rotate3d(${i}, calc(1 - ${i}), 0, calc(${m} * 90deg)) translateZ(20px);
+		`;
+	}}
+
+	& > span {
+		position: absolute;
+
+		height: 2px;
+		width: 100%;
+		background-color: ${(p) => p.theme.colors.deepBlue};
+		display: block;
+
+		${addTransition()}
+		${({ faceVisible }) => (faceVisible ? 'opacity: 1;' : 'opacity: 0;')}
+
+		&:nth-child(1) {
+			margin-bottom: 20px;
+		}
+
+		&:nth-child(3) {
+			margin-top: 20px;
+		}
+
+		${({ faceIndex }) => {
+			if (faceIndex % 2 !== 0) {
+				return css`
+					&:nth-child(1) {
+						transform: rotateZ(0deg);
+					}
+					&:nth-child(3) {
+						transform: rotateZ(0);
+					}
+				`;
+			}
+
+			return css`
+				&:nth-child(1) {
+					transform: rotateZ(-45deg);
+					margin-bottom: 0;
+				}
+
+				&:nth-child(2) {
+					width: 0;
+				}
+
+				&:nth-child(3) {
+					margin-top: 0;
+					transform: rotateZ(45deg);
+				}
+			`;
+		}};
 	}
 `;
--- packages/frontend/components/Navbar/Navbar.tsx
+++ packages/frontend/components/Navbar/Navbar.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useReducer, useState } from 'react';
+import useResponsive from '../../hooks/useResponsive';
 import Flex from '../../styles/layout/Flex';
 import Spacing from '../../styles/layout/Spacing';
 import BMButton from '../../styles/theme/components/BMButton';
 import BMText from '../../styles/theme/components/BMText';
 import { EFlex, ESize, ETextColor, ETextType, ETextWeight } from '../../styles/theme/utils/enum';
-import { StyledNavbar, StyledNavbarItem, StyledList } from './Navbar.styles';
+import {
+	StyledNavbar,
+	StyledNavbarItem,
+	StyledList,
+	StyledBurgerButton,
+	StyledFace,
+	StyledCube
+} from './Navbar.styles';
 
 export const NAVBAR_LINKS = {
 	home: {
@@ -36,7 +44,12 @@ export const INTERNAL_LINKS = {
 
 const Navbar = () => {
 	const router = useRouter();
+	const { isSmallerThanMd } = useResponsive();
+
 	const { pathname } = router;
+
+	const [navbarBlurred, setNavbarBlurred] = useState(false);
+	const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
 
 	const navbarHidden = useMemo(() => {
 		if (pathname === INTERNAL_LINKS.story.href) {
@@ -46,7 +59,59 @@ const Navbar = () => {
 		return false;
 	}, [pathname]);
 
-	const [navbarBlurred, setNavbarBlurred] = useState(false);
+	const navbarLinks = useMemo(() => {
+		return (
+			<StyledList>
+				{Object.values(NAVBAR_LINKS).map(({ label, href }) => (
+					<StyledNavbarItem key={href}>
+						<Link href={href}>
+							<a>
+								<BMText
+									type={ETextType.span}
+									inheritStyle={false}
+									weight={ETextWeight.light}
+									size={ESize.m}
+									className={pathname === href ? 'navbar-active' : ''}
+									textColor={pathname === href ? ETextColor.light : ETextColor.default}
+								>
+									{label}
+								</BMText>
+							</a>
+						</Link>
+					</StyledNavbarItem>
+				))}
+			</StyledList>
+		);
+	}, [pathname]);
+
+	const [burgerClickCount, incrementBurgerClickCount] = useReducer((count) => {
+		if (count + 1 > 5) {
+			return 0;
+		} else {
+			return count + 1;
+		}
+	}, 0);
+
+	const handleBurgerClick = useCallback(() => {
+		incrementBurgerClickCount();
+		setBurgerMenuOpen(!burgerMenuOpen);
+	}, [burgerMenuOpen, incrementBurgerClickCount]);
+
+	const burgerButton = useMemo(() => {
+		return (
+			<StyledBurgerButton onClick={() => handleBurgerClick()}>
+				<StyledCube value={burgerClickCount}>
+					{Array.from({ length: 6 }, (_, i) => (
+						<StyledFace faceIndex={i + 1} faceVisible={burgerClickCount === i}>
+							<span />
+							<span />
+							<span />
+						</StyledFace>
+					))}
+				</StyledCube>
+			</StyledBurgerButton>
+		);
+	}, [burgerMenuOpen, burgerClickCount]);
 
 	useEffect(() => {
 		// detect scroll and call blurNavbar after 100 px from top
@@ -68,31 +133,12 @@ const Navbar = () => {
 
 	return (
 		<StyledNavbar isHidden={navbarHidden} isBlurred={navbarBlurred}>
-			<Flex vertical={EFlex.center}>
+			<Flex vertical={EFlex.center} mdHorizontal={EFlex.between}>
 				<div className='logo' />
 
 				<Spacing size={ESize.xl} />
 
-				<StyledList>
-					{Object.values(NAVBAR_LINKS).map(({ label, href }) => (
-						<StyledNavbarItem key={href}>
-							<Link href={href}>
-								<a>
-									<BMText
-										type={ETextType.span}
-										inheritStyle={false}
-										weight={ETextWeight.light}
-										size={ESize.m}
-										className={pathname === href ? 'navbar-active' : ''}
-										textColor={pathname === href ? ETextColor.light : ETextColor.default}
-									>
-										{label}
-									</BMText>
-								</a>
-							</Link>
-						</StyledNavbarItem>
-					))}
-				</StyledList>
+				{isSmallerThanMd ? <>{burgerButton}</> : <>{navbarLinks}</>}
 			</Flex>
 
 			<BMButton size={ESize.m} secondary onClick={() => alert('Work in progress')}>
