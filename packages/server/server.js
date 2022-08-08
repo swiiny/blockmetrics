6581aaffb20493451a0ae98ebf9b861062f673e1
Jeremy Theintz <jeremy@appogee.ch>
2022-08-08 10:50:23 +0200

    fix check if addresses are contracts issue

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { getNodeCountForAllBlockchains } from './utils/fetch/posNodeCount.js';
-import { calculatePowerConsumptionPoS, getRankFromScore, getWsRpcByChainId } from './utils/functions.js';
+import { calculatePowerConsumptionPoS, getRankFromScore, getRpcByChainId } from './utils/functions.js';
 import { createDbPool } from './utils/pool/pool.js';
 import {
 	getPowerConsumptionDataForPoS,
@@ -637,7 +637,8 @@ async function checkIfAddressesAreContracts(con) {
 		console.log('resolvedAccountRows', resolvedAccountRows.length);
 
 		const txPromises = resolvedAccountRows.map(async ({ blockchain_id, address }) => {
-			const provider = new ethers.providers.WebSocketProvider(getWsRpcByChainId(blockchain_id));
+			const provider = new ethers.providers.JsonRpcProvider(getRpcByChainId(blockchain_id));
+			//const provider = new ethers.providers.WebSocketProvider(getWsRpcByChainId(blockchain_id));
 
 			return provider.getCode(address).then((res) => {
 				if (res === '0x') {
